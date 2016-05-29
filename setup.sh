9c2f0d7b1c4500da45f1991045c0776dedf6549f
Sidney Fong <sydneyfong@localhost>
2016-05-29 18:27:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,16 @@
 # makes the symlinks for you
 
 cd ~/
-ln -s skel*/_bashrc .bashrc
-ln -s skel*/_bash_profile .bash_profile
-ln -s skel*/_vimrc .vimrc
-ln -s skel*/_vim .vim
-ln -s skel*/_screenrc .screenrc
+if [[ ! -d skel/.git ]]; then
+  echo "Put the skel/ directory in your home directory!" >&2
+  exit 1
+fi
+ln -s skel/_bashrc .bashrc
+ln -s skel/_bash_profile .bash_profile
+ln -s skel/_vimrc .vimrc
+ln -s skel/_vim .vim
+ln -s skel/_screenrc .screenrc
 touch ~/.hnfong.conf
 
+mkdir -p ~/bin/
+ln -s ../skel/bin/charcount ~/bin/
