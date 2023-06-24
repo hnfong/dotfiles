d0aca22b0797da818f3370c6b998cff8926ca814
Sidney Fong <sydneyfong@localhost>
2023-06-24 20:42:15 +0800

    New shell scripts

---
+++ bin/ffmpeg-cap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for ((min=0;min<60;min++)); do
+    for sec in 00 05 10 15 20 25 30 35 40 45 50 55; do
+        ffmpeg -ss "$min":"$sec" -i "$1" -frames:v 1 -q:v 2 "$1"-"$min"."$sec".jpg
+    done
+done
