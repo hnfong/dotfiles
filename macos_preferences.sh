63cabc6aad07cabe42d719e532cb1656bb56d304
Sidney Fong <sydneyfong@localhost>
2026-06-18 01:17:26 +0800

    Update

--- macos_preferences.sh
+++ macos_preferences.sh
@@ -16,3 +16,8 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
+
+# This contributes to an issue with World of Warcraft tabbing out to desktop occasionally in full screen mode.
+# Suggested by Gemini after I asked it how to disable the blue caps lock thingy that pops up
+# https://stackoverflow.com/questions/77248249/disable-macos-sonoma-text-insertion-point-cursor-caps-lock-indicator
+sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
